429810735e874efe17e04976d29300ab7e7d2516
arthur-es <arthurcastroes@gmail.com>
2020-01-29 20:55:31 -0300

    nodemon sucrase: bug fix

--- modulo2/src/routes.js
+++ modulo2/src/routes.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const routes = new Router();
 
 routes.get("/", (req, res) => {
-    return res.json("Hello");
+    return res.json("Hello World 3");
 });
 
 export default routes;
