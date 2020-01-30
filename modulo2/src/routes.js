fec0c8e4e980bb0076dee9905773e44085c5accd
arthur-es <arthurcastroes@gmail.com>
2020-01-30 20:30:27 -0300

    estrutura de pastas + config do sequelize

--- modulo2/src/routes.js
+++ modulo2/src/routes.js
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-    return res.json("Hello World 3");
+routes.get('/', (req, res) => {
+  return res.json('Hello World 3');
 });
 
 export default routes;
