025ea98a575253f51ec511380803c448ee0c862e
arthur-es <arthurcastroes@gmail.com>
2020-02-03 18:04:13 -0300

    adicionado hash da senha

--- modulo2/src/app/models/User.js
+++ modulo2/src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -6,6 +7,7 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL, // NÃO EXISTE NO BD
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
       },
@@ -13,6 +15,17 @@ class User extends Model {
         sequelize,
       }
     );
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        console.log('Encriptando a senha...');
+
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      } else {
+        console.log('Não encriptou');
+      }
+    });
+
+    return this;
   }
 }
 
