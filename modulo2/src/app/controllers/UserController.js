84471142a44dd579196b8c26273d7c9dd61d7d57
arthur-es <arthurcastroes@gmail.com>
2020-02-03 21:16:37 -0300

    validation with Yup

--- modulo2/src/app/controllers/UserController.js
+++ modulo2/src/app/controllers/UserController.js
@@ -1,7 +1,17 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object.shape({
+      name: Yup.string().require(),
+      email: Yup.email().required(),
+      password: Yup.string().min(6),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ error: 'validation failed' });
+    }
+
     const userExists = await User.findOne({ where: { email: req.body.email } });
     if (userExists) {
       return res.status(400).json({ error: 'User already exists.' });
@@ -11,6 +21,22 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object.shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.require() : field
+        ),
+      confirmPassword: Yup.string().when((password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ error: 'validation failed at update' });
+    }
     const { email, oldPassword } = req.body;
     const user = await User.findByPk(req.userId);
 
